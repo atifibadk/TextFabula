bb709e92260704179e1d3528bc703e68d2719ffb
ibad <atifibadk@gmail.com>
2025-02-22 00:30:40 +0530

    conversion optmisation

--- python_cross_Server.py
+++ python_cross_Server.py
@@ -1,18 +1,15 @@
 import http.server
-import ssl
+import socketserver
 
 PORT = 8000
 
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
+        # Add the cross-origin isolation headers
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         super().end_headers()
 
-httpd = http.server.HTTPServer(("192.168.1.45", PORT), CustomRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile="cert.pem", keyfile="key.pem", server_side=True)
-
-print(f"Serving HTTPS at https://localhost:{PORT}")
-httpd.serve_forever()
-
-
+with socketserver.TCPServer(("", PORT), CustomRequestHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
